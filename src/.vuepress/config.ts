e85e61527a448ef169b7cf6dcbcb40298804d2f4
wangchen <3354599625@qq.com>
2025-02-12 14:30:53 +0800

    initial commit

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/Tekapo-myBlog/",
+  base: "/",
 
   lang: "zh-CN",
   title: "特卡波的个人博客",
